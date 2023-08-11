98d7b1c7a04132f7cc73cd4764f03f342bd9670e
Nedeljko Babic <bnedeljko0@gmail.com>
2023-08-11 19:55:49 +0200

    created another activity for for log in

---
+++ app/src/main/java/com/example/studenthealthcare/ui/LogInActivity.kt
@@ -0,0 +1,17 @@
+package com.example.studenthealthcare.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.SpannableString
+import com.example.studenthealthcare.R
+
+class LogInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_log_in)
+
+        val stringcina : SpannableString = SpannableString("Ide gas")
+
+
+    }
+}
