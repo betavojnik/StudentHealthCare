6981b9e43319c98aa7e1d16ce870307e26cc5887
Nedeljko Babic <bnedeljko0@gmail.com>
2023-08-14 17:50:26 +0200

    hyper link now works properly

--- app/src/main/java/com/example/studenthealthcare/ui/LogInActivity.kt
+++ app/src/main/java/com/example/studenthealthcare/ui/LogInActivity.kt
@@ -1,17 +1,44 @@
 package com.example.studenthealthcare.ui
 
+import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.SpannableString
+import android.text.style.ClickableSpan
+import android.view.View
+import androidx.core.content.ContentProviderCompat.requireContext
 import com.example.studenthealthcare.R
+import com.example.studenthealthcare.databinding.ActivityLogInBinding
+import com.google.android.material.snackbar.Snackbar
+import com.klinker.android.link_builder.Link
+import com.klinker.android.link_builder.applyLinks
 
 class LogInActivity : AppCompatActivity() {
+
+     lateinit var binding : ActivityLogInBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_log_in)
+        binding = ActivityLogInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val stringcina : SpannableString = SpannableString("Ide gas")
+        linkSetup()
 
+    }
 
+    private fun linkSetup() {
+
+        val anotherActivity = Link("Register now!")
+            .setTextColor(Color.BLUE)
+            .setUnderlined(true)
+            .setOnClickListener {
+                 Intent(this, MainActivity::class.java).also {
+                     startActivity(it)
+                 }
+            }
+
+        binding.textView.applyLinks(anotherActivity)
     }
 }
